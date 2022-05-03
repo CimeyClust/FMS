881d11413d524423c7026866641f432f6264d3ee
JanWeinsheimer <102093266+JanWeinsheimer@users.noreply.github.com>
2022-05-03 16:10:12 +0200

    Add files via upload

---
+++ Controller/Control.py
@@ -0,0 +1,36 @@
+#from View import *
+#from Model import *
+
+class Controller(object):
+    #def __init__(self):
+        #self.model = Model()
+        #self.view = View()
+    
+    def alleExemplare(self):
+        # vom model werden die Werte für alle Exemplare geholt
+        # tabelle = model.get_alleBuecher()
+        
+        # die tabelle wird formatiert
+        
+        
+        # es wird dem View zurückgegeben
+        # Titel, Fachbereich, ExemplarID, eventueller Name des Schülers
+        return tabelle
+    
+    def entliehendeExemplare(self):
+        # vom Model werden die Werte für Exemplare geholt, welche gerade entliehen sind
+        # tabelle = model.get_entliehendeBuecher()
+        
+        # die tabelle wird formatiert
+        
+        
+        return tabelle
+    
+    def entleihbareExemplare(self):
+        # vom Model werden die Werte für Exemplare geholt, welche gerade entleihbar sind
+        # tabelle = model.get_entleihbareBuecher()
+        
+        # die tabelle wird formatiert
+        
+        
+        return tabelle
