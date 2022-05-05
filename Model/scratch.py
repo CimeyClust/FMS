91b1222929aa6154ddc7de341337c98b16351fd0
Yapollon <75550334+Yapollon@users.noreply.github.com>
2022-05-05 18:58:31 +0200

    Model Update
    BIG CHANGES:
    Method to print out the full content of all tables. Makes it much easier to check changes in the database.
    
    Insert Method and Get Method got a revamp.
    
    small changes:
    improvements to the table generation.

--- Model/scratch.py
+++ Model/scratch.py
@@ -1,15 +1,19 @@
-    def toCSV(self):
-        connection = sqlite3.connect('fms.db')
-        cursor = connection.cursor()
+import sqlite3
+import pandas as pd
 
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cursor.fetchall()
-        for table_name in tables:
-            table_name = table_name[0]
-            table = pd.read_sql_query(f"SELECT * from {table_name}", connection)
-            table.to_csv(table_name + '.csv', index_label='index')
-            print("\n" + table_name)
-            print(table)
 
-        cursor.close()
-        connection.close()
+def toCSV(self):
+    connection = sqlite3.connect('fms.db')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    for table_name in tables:
+        table_name = table_name[0]
+        table = pd.read_sql_query(f"SELECT * from {table_name}", connection)
+        table.to_csv(table_name + '.csv', index_label='index')
+        print("\n" + table_name)
+        print(table)
+
+    cursor.close()
+    connection.close()
