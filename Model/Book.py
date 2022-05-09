acb66be4918911a2a50ed3389b76b9ced36dd80d
Verox001 <verox@cimeyclust.com>
2022-05-09 14:42:10 +0200

    Class Student
    Added a class for studendts and fuction getStudents()

--- Model/Book.py
+++ Model/Book.py
@@ -1,9 +1,14 @@
+from Model.Title import Title
+from Model.Student import Student
+
 books = []
 
 class Book:
-    def __init__(self, id: int, borrowed: bool):
+    def __init__(self, id: int, borrowed: bool, title: Title, user: Student = None):
         self.id = id
         self.borrowed = borrowed
+        self.title = title
+        self.user = user
 
         books.append(self)
 
