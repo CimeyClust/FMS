2ad7251b99fbe991e7152d125e5ea83404dc229d
Luis <75887681+Luis-Hamann@users.noreply.github.com>
2022-05-16 15:01:30 +0200

    Update SQLiteModel.py

--- Model/SQLiteModel.py
+++ Model/SQLiteModel.py
@@ -154,7 +154,7 @@ class Model(object):
         return self.cursor.fetchone()
 
     def getStudentIDs(self):
-        self.cursor.execute("SELECT BenutzerID FROM Benutzer;")
+        self.cursor.execute("SELECT BenutzerID FROM BENUTZER;")
         return self.cursor.fetchall()
 
     def getStudentSurName(self, benutzer_id: int):                 #-> str
