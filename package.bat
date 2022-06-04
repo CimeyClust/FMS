9e3419703066498d78d2a26ba12d8ad9dd7c891d
Verox001 <verox@cimeyclust.com>
2022-06-04 22:59:01 +0200

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-"venv/Scripts/pyinstaller.exe" -y --noconsole --add-data "venv/Lib/site-packages/customtkinter;customtkinter/" --onefile -- --add-data "View/images/*.png;View/images" --paths "venv/Lib/site-packages/" FMS.py
+"venv/Scripts/pyinstaller.exe" -y --noconsole --add-data "venv/Lib/site-packages/customtkinter;customtkinter/" --add-data "View/images/*.png;View/images" --paths "venv/Lib/site-packages/" FMS.py
