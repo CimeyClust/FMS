e27776f9ca161420124a47c129ea124cf6be24f0
Verox001 <verox@cimeyclust.com>
2022-06-08 18:22:03 +0200

    Stable Release 1.1.2 Update

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-"venv/Scripts/pyinstaller.exe" -y --noconsole --add-data "venv/Lib/site-packages/customtkinter;customtkinter/" --add-data "View/images/*.png;View/images" --paths "venv/Lib/site-packages/" FMS.py
+"venv/Scripts/pyinstaller.exe" -y --icon="View/images/logo.ico" --noconsole --add-data "venv/Lib/site-packages/customtkinter;customtkinter/" --add-data "View/images/*.png;View/images" --add-data "View/images/*.ico;View/images" --paths "venv/Lib/site-packages/" FMS.py
